6b1f40e8b7c7eb034406be593cd4b1a17c0dcae8
tungtv <tungtv@Ubuntu-01.myguest.virtualbox.org>
2023-08-29 00:20:13 +0700

    logic to verify credential

--- authorization.py
+++ authorization.py
@@ -1,11 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, session, jsonify
 from markupsafe import escape
 from flask_cors import CORS
 import psycopg2
+from itsdangerous import TimedSerializer, BadSignature, SignatureExpired
 
 
 app = Flask(__name__)
 CORS(app)
+app.secret_key = 'abc123'
+
+#Set the token expiration time (in seconds)
+TOKEN_EXPIRATION_TIME = 3600 #1 hour
 
 
 def create_connection():
@@ -22,20 +27,6 @@ def create_connection():
         password=password
     )
     return connection
-
-
-# @app.route('/')
-# def test_db_connection():
-#     try:
-#         connection = create_connection()
-#         cursor = connection.cursor()
-#         cursor.execute('SELECT version()')
-#         db_version = cursor.fetchone()[0]
-#         cursor.close()
-#         connection.close()
-#         return f'Successfully connected to PostgreSQL. Database version: {db_version}'
-#     except (Exception, psycopg2.Error) as error:
-#         return f'Error connecting to PostgreSQL: {error}'
 
 
 @app.route('/users', methods=['GET', 'POST'])
@@ -63,38 +54,59 @@ def registration():
             }
 
 
-    # if request.method == 'POST':
-    #     # logic to push data to DB
-    #     return {'First Name: ': user['firstName'],
-    #             'Last Name: ': user['lastName'],
-    #             'Email: ': user['email'],
-    #             'Password: ':user['password']
-    #             }
-    # else:
-    #     return ['Wrong http method']
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     user = request.get_json()
     email = user['email']
     password = user['password']
 
+    if verify_credentials(email, password):
+        # token = generate_token(email)
+        # session['token'] = token
+        return jsonify({'success': True, 'message': 'Login successful.'})
+    else:
+        return jsonify({'success': False, 'message': 'Invalid username or password.'})
+
+def generate_token(email):
+    serializer = TimedSerializer(app.secret_key, expires_in=TOKEN_EXPIRATION_TIME)
+    data = {'email': email}
+    token = serializer.dumps(data).decode('utf-8')
+    return token
+
+def verify_credentials(email, password):
     connection = create_connection()
     cursor = connection.cursor()
-
     insert_query = 'SELECT email FROM users WHERE email = %s AND password = %s'
     cursor.execute(insert_query, (email, password))
     result = cursor.fetchone()
-
     if result:
-        return {'Login status: ': 'Login successful'}
+        return True
     else:
-        return {'Login status: ': 'Login failed'}
+        return False
 
     cursor.close()
     connection.close()
 
+
+@app.route('/protected')
+def protected():
+    token = session.get('token')
+    if token:
+        data = verify_and_decode_token(token)
+        if data:
+            email = data['email']
+            return f'Projected content for user: {email}' #recheck
+    return 'Unauthorized' #recheck
+
+def verify_and_decode_token(token):
+    serializer = TimedSerializer(app.secret_key)
+    try:
+        data = serializer.loads(token)
+        return data
+    except SignatureExpired:
+        return None
+    except BadSignature:
+        return None
 
 # if __name__ == '__main__':
 #     app.run()
